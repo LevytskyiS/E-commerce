d476ac0aadac7272853012af5eb8d225523fb21e
Serhii Levytskyi <112187090+LevytskyiS@users.noreply.github.com>
2024-04-11 22:37:50 +0200

    "Add order list view and update user detail template

--- eshop/orders/urls.py
+++ eshop/orders/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 app_name = "orders"
 urlpatterns = [
+    path("", views.OrderListView.as_view(), name="order_list"),
     path("order/<slug:slug>/", views.OrderDetailView.as_view(), name="order_detail"),
 ]
--- eshop/orders/views.py
+++ eshop/orders/views.py
@@ -1,3 +1,5 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.views.generic import (
     ListView,
@@ -11,9 +13,17 @@ from django.views.generic import (
 from .models import Order
 
 
-# Create your views here.
 # Order
 class OrderDetailView(DetailView):
     model = Order
     template_name = "orders/order_detail.html"
     context_object_name = "order"
+
+
+class OrderListView(ListView):
+    model = Order
+    template_name = "orders/order_list.html"
+    context_object_name = "orders"
+
+    def get_queryset(self) -> QuerySet[Order]:
+        return Order.objects.filter(user=self.request.user).order_by("created_at")
