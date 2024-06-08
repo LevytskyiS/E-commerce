3512890be995f6f1904a5304f19ba76f867f91de
Serhii Levytskyi <112187090+LevytskyiS@users.noreply.github.com>
2024-06-08 21:19:08 +0200

    "Updated settings.py to include STATICFILES_DIRS, added new CSS styles in style.css, and modified HTML templates in base.html and user_detail.html to include new profile page layout and design elements."

--- eshop/eshop/settings.py
+++ eshop/eshop/settings.py
@@ -167,6 +167,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+# staticfiles_dirs нужен, чтобы находить static файлы во всех апках
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
